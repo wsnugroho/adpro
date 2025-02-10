a26b6277d2ab8740059e172135913970d59d018c
Wisnu Nugroho <wisnunugroho614@gmail.com>
2025-02-11 06:16:22 +0700

    feat: add product model

---
+++ src/main/java/id/ac/ui/id/cs/adprog/eshop/model/Product.java
@@ -0,0 +1,13 @@
+package id.ac.ui.id.cs.adprog.eshop.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private String productId;
+    private String productName;
+    private int productQuantity;
+}
