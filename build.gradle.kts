301287879306077f9990f97a2bc7a78647c318a7
Wisnu Nugroho <wisnunugroho614@gmail.com>
2025-02-20 22:02:56 +0700

    feat: use csui sonarqube for code analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "id.ac.ui.id.cs.adprog"
@@ -16,6 +17,13 @@ val junitJupiterVersion = "5.9.1"
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "ADVPROG-Eshop")
+        property("sonar.projectName", "ADVPROG-Eshop")
     }
 }
 
