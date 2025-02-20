a57e319472c114e9c53697ca5ad25b506ce8e66a
Wisnu Nugroho <wisnunugroho614@gmail.com>
2025-02-20 21:51:55 +0700

    feat: 100% coverage eshop application

--- src/test/java/id/ac/ui/id/cs/adprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/id/cs/adprog/eshop/EshopApplicationTests.java
@@ -1,13 +1,25 @@
 package id.ac.ui.id.cs.adprog.eshop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class EshopApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext);
     }
 
+    @Test
+    void mainMethodStartsApplication() {
+        EshopApplication.main(new String[]{});
+    }
 }
