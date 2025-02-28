6ffe1ca99c5d83504ce89e320c2e462a1bd1eac9
Wisnu Nugroho <wisnunugroho614@gmail.com>
2025-02-28 13:04:44 +0700

    refactor: move CarController to its own file

---
+++ src/main/java/id/ac/ui/id/cs/adprog/eshop/controller/CarController.java
@@ -0,0 +1,58 @@
+package id.ac.ui.id.cs.adprog.eshop.controller;
+
+
+import id.ac.ui.id.cs.adprog.eshop.model.Car;
+import id.ac.ui.id.cs.adprog.eshop.service.CarServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/car")
+public class CarController {
+    @Autowired
+    private CarServiceImpl carService;
+
+    @GetMapping("/listCar")
+    public String carListPage(Model model) {
+        List<Car> allCars = carService.findAll();
+        model.addAttribute("cars", allCars);
+        return "car/listCar";
+    }
+
+    @GetMapping("/createCar")
+    public String createCarPage(Model model) {
+        Car car = new Car();
+        model.addAttribute("car", car);
+        return "car/createCar";
+    }
+
+    @PostMapping("/createCar")
+    public String createCarPost(@ModelAttribute Car car, Model model) {
+        carService.create(car);
+        return "redirect:/car/listCar";
+    }
+
+    @GetMapping("/editCar/{carId}")
+    public String editCarPage(@PathVariable String carId, Model model) {
+        Car car = carService.findById(carId);
+        model.addAttribute("car", car);
+        return "car/editCar";
+    }
+
+    @PostMapping("/editCar")
+    public String editCarPost(@ModelAttribute Car car, Model model) {
+        System.out.println(car.getCarId());
+        carService.updateById(car.getCarId(), car);
+        return "redirect:/car/listCar";
+    }
+
+    @PostMapping("/deleteCar")
+    public String deleteCar(@RequestParam String carId, Model model) {
+        carService.deleteById(carId);
+        return "redirect:/car/listCar";
+    }
+}
